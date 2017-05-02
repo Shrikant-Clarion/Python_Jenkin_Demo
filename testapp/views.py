8825a3bbd6508601be35876dd5e8bb67f428dd63
Shrikant-Clarion <shrikant.joshi@clariontechnologies.co.in>
2017-05-02 18:40:09 +0530

    testcases and jenkin integration

--- testapp/views.py
+++ testapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
-    #return HttpResponse("Hello, world. Jenkin Integration with Github successfully done.")
-    return HttpResponse("Jenkin Integration with Github successfully done.")
+    return HttpResponse("Hello, world. Jenkin Integration with Github successfully done.")
+    #return HttpResponse("Jenkin Integration with Github successfully done.")
